a7ca153ca1b1a10793e3ff367e631345ed302a0d
임재원 <po963069@gmail.com>
2022-12-08 22:47:49 +0900

    시작 화면 완성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
+import { useState } from 'react';
 import './App.css';
-function Main(){
-  return<article>
-    <button>start</button>
-  </article>
+
+
+function Main(props){
+  return <div id='Main'> 
+    <button onClick={props.ModeChange}>start</button>
+  </div>
+}
+
+function InGame(){
+  return <div id = 'InGame'>
+      <button>STOP</button>
+    </div>
 }
 
 
 function App() {
+
+  const [mode, setmode] = new useState('Main');
+  let content = null;
+
+  if(mode === 'Main')
+  {
+    content = <Main ModeChange = {() => setmode("InGame")}></Main>
+  }
+  else if(mode === 'InGame')
+  {
+    content = <InGame></InGame>
+  }
+
   return (
-    <Main></Main>
+    <div>
+      <header>
+        <h1>Fill The Bottle</h1>
+      </header>
+      {content}
+      <img src = {require("./images/bottle.png")} alt = 'bottle'></img>
+    </div>
   );
 }
 
